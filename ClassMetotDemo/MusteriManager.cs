2055fd474dac1af82a14caad74167fccb093cd1d
Yasin SANDIKÇI <ysnsndkc@hotmail.com>
2022-01-26 14:53:28 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " listeye eklendi."); //ekleme simülasyonu   
+        }
+        public void Cıkar(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " listeden çıkarıldı."); // çıkartma simülasyonu
+        }
+
+        public void List(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " müşteri yaşı : " + musteri.Yasi);
+            }
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 01;
+            musteri1.Adi = "Yasin";
+            musteri1.Soyadi = "Sandıkçı";
+            musteri1.Yasi = 27;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 02;
+            musteri2.Adi = "İrfan";
+            musteri2.Soyadi = "Nişancı";
+            musteri2.Yasi = 25;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 03;
+            musteri3.Adi = "Murat";
+            musteri3.Soyadi = "Senol";
+            musteri3.Yasi = 29;
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Id = 04;
+            musteri4.Adi = "Duhan";
+            musteri4.Soyadi = "İrfanoğlu";
+            musteri4.Yasi = 28;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3, musteri4 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri3);
+            musteriManager.Cıkar(musteri2);
+            musteriManager.List(musteriler);
+            musteriManager.Cıkar(musteri4);
+
+
+        }
+    }
+}
